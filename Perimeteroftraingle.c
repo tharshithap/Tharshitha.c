fd21534f083ae7344c4a8eda1b81fdcfb371bd50
tharshithap <sujipas722@gmail.com>
2024-09-30 19:52:31 +0530

    Create Perimeteroftraingle.c

---
+++ Perimeteroftraingle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+  int l,b,perimeter;
+  printf("enter the length and breadth :");
+  scanf("%d%d",&l,&b);
+  perimeter = (2*(l+b));
+  printf("the perimeter of triangle %d",perimeter);
+  
+
+    return 0;
+}
