87d180c8ce1921de020d2718ed6d77139dbe5942
tharshithap <sujipas722@gmail.com>
2025-01-10 11:49:34 +0530

    Create reverseorderarrayelements.c

---
+++ reverseorderarrayelements.c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+int main() 
+{
+    int a[10],i,n;
+    printf("enter the size:");
+    scanf("%d",&n);
+    printf("enter the elements:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=n-1;i>0;i--)
+    {
+    printf("%d\t",a[i]);
+    } 
+  }
